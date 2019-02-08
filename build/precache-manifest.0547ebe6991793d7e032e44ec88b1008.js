d37d938fdcbd09b5302f5892d5aa360d954285a4
Vikke Tiirola <vikke.tiirola@gmail.com>
2019-02-08 17:43:25 +0200

    added frontend build

---
+++ build/precache-manifest.0547ebe6991793d7e032e44ec88b1008.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5e03894f83327a12dda",
+    "url": "/static/js/main.a5e03894.chunk.js"
+  },
+  {
+    "revision": "b83b541bf9a21fe0053c",
+    "url": "/static/js/1.b83b541b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "528ef345c31c961c29f1c17f8c8b5694",
+    "url": "/index.html"
+  }
+];
